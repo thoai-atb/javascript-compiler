70afac0f4dba431154a1c4bd5619e000040f8dbf
Thoai Ly <lybaothoai@gmail.com>
2021-05-20 15:27:28 +0700

    generate a log file for each input file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import src as js_compiler
 
 file_path = input('Enter an input file name to be executed: ')
-result, error = js_compiler.run('input/' + file_path, 'log.txt')
+result, error = js_compiler.run('input/' + file_path + '.js', 'input/' + file_path + '[log].txt')
 if error:
     print(error.as_string())
 else:
